f2a2f5c8a9cf9266d26e149e5d71eb73c5621e5a
Anton Kapelyushok <a.kapelyushok@gmail.com>
2022-11-10 21:09:48 +0100

    firefox selenium

--- src/main/kotlin/poupa/realty/amsterdam/AmsterdamRealtyApplication.kt
+++ src/main/kotlin/poupa/realty/amsterdam/AmsterdamRealtyApplication.kt
@@ -4,6 +4,11 @@ import com.pengrad.telegrambot.TelegramBot
 import com.pengrad.telegrambot.request.SendMessage
 import mu.KotlinLogging
 import org.jsoup.Jsoup
+import org.openqa.selenium.By
+import org.openqa.selenium.Point
+import org.openqa.selenium.WebDriver
+import org.openqa.selenium.firefox.FirefoxDriver
+import org.openqa.selenium.firefox.FirefoxOptions
 import org.springframework.boot.ApplicationArguments
 import org.springframework.boot.ApplicationRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
@@ -11,9 +16,6 @@ import org.springframework.boot.runApplication
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.dao.DuplicateKeyException
-import org.springframework.data.annotation.Id
-import org.springframework.data.relational.core.mapping.Table
-import org.springframework.data.repository.CrudRepository
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate
 import org.springframework.scheduling.annotation.EnableScheduling
 import org.springframework.scheduling.annotation.Scheduled
@@ -25,11 +27,15 @@ import java.net.URI
 import java.net.http.HttpClient
 import java.net.http.HttpRequest
 import java.net.http.HttpResponse.BodyHandlers
+import java.time.Duration
+import java.util.concurrent.TimeUnit
 import kotlin.random.Random
+
 
 private val log = KotlinLogging.logger {}
 
 val chatId = -716389843
+val meId = 128947731
 
 val token = System.getProperty("TG_TOKEN") ?: runCatching { File("./.token").readText() }.getOrNull()
 ?: error("could not find token")
@@ -47,15 +53,28 @@ class TelegramBotConfiguration {
 class AmsterdamRealtyApplication(
     private val bot: TelegramBot,
     private val fundaListingsFetcher: FundaListingsFetcher,
+    private val parariusListingsFetcher: ParariusListingsFetcher,
 ) : ApplicationRunner {
 
 
     override fun run(args: ApplicationArguments?) {
-//        val res = fundaListingsFetcher.fetch()
+        System.setProperty(
+            "webdriver.gecko.driver",
+            "/Users/akapelyushok/Projects/amsterdam-realty/lib/geckodriver"
+        )
+//        val res1 = bot.execute(SendSilentMessage(meId, "I'm alive!"))
+//        println(res)
+//        val res = parariusListingsFetcher.fetch()
+//        res
 //        val req = SendMessage(-716389843, "Hello, world")
 //        val res = bot.execute(GetUpdates())
-//        val res = bot.execute(req)
+//        println(res)
+
     }
+}
+
+fun SendSilentMessage(chatId: Any, text: String) = SendMessage(chatId, text).apply {
+    parameters["disable_notification"] = true
 }
 
 @Component
@@ -65,7 +84,8 @@ class CronChecker(
     private val jdbcTemplate: NamedParameterJdbcTemplate,
     private val telegramBot: TelegramBot,
 ) {
-    @Scheduled(cron = "0 */5 * * * *")
+    //    @Scheduled(cron = "0 */5 * * * *")
+    @Scheduled(fixedDelay = 5, timeUnit = TimeUnit.MINUTES)
     fun runUpdate() {
         val listings = listOf(
             parariusListingsFetcher,
@@ -74,7 +94,7 @@ class CronChecker(
             try {
                 val res = it.fetch()
                 if (res.isEmpty()) {
-                    telegramBot.execute(SendMessage(chatId, "${it::class.java} returned empty result"))
+                    telegramBot.execute(SendSilentMessage(meId, "${it::class.java} returned empty result"))
                 }
                 res
             } catch (e: Throwable) {
@@ -82,7 +102,7 @@ class CronChecker(
                 val sw = StringWriter()
                 e.printStackTrace(PrintWriter(sw))
                 val exceptionAsString = sw.toString()
-                telegramBot.execute(SendMessage(chatId, exceptionAsString))
+                telegramBot.execute(SendMessage(meId, exceptionAsString))
                 listOf()
             }
         }
@@ -141,26 +161,54 @@ data class Listing(
 )
 
 @Component
-class ParariusListingsFetcher : ListingsFetcher {
+class ParariusListingsFetcher(
+    private val driverFactory: DriverFactory,
+) : ListingsFetcher {
     override fun fetch(): List<Listing> {
-        val link = "https://www.pararius.nl/huurwoningen/amsterdam/1500-2000?query=${Random.nextInt(0, 1000000)}"
-        val request = HttpRequest.newBuilder().uri(URI.create(link)).header(
-            "accept",
-            """text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9"""
-        ).header(
-            "cookie",
-            "latest_search_locations=%5B%22amsterdam%22%5D; path=/; domain=.www.pararius.nl; secure; samesite=lax"
-        ).header(
-            "user-agent",
-            """Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/${
-                Random.nextInt(
-                    400, 600
-                )
-            }.35"""
-        ).build();
-        val client = HttpClient.newBuilder().build()
-        val text = client.send(request, BodyHandlers.ofString()).body()
-//        val text = File("./pararius.html").readText()
+
+        val webDriver = driverFactory.webDriver()
+        val text = try {
+            webDriver.navigate().to("https://www.pararius.nl/huurwoningen/amsterdam/1500-2000")
+            webDriver.manage().window().position = Point(-100, -1000);
+
+            val start = System.currentTimeMillis()
+            while (true) {
+                try {
+                    val now = System.currentTimeMillis()
+                    if (Duration.ofMillis(now - start) > Duration.ofSeconds(10)) {
+                        log.info { "Pararius timeout :(" }
+                        return emptyList()
+                    }
+                    try {
+                        webDriver.findElement(By.className("search-list-sorting"))
+                        break
+                    } catch (e: org.openqa.selenium.NoSuchElementException) {
+                        Thread.sleep(300)
+                        continue
+                    }
+                } catch (e: InterruptedException) {
+                    Thread.currentThread().interrupt()
+                    return listOf()
+                } catch (e: Throwable) {
+                    // other exception
+                    throw e;
+                }
+            }
+
+            webDriver.pageSource
+        } finally {
+            for (i in 1..10) {
+                try {
+                    webDriver.quit()
+                    break
+                } catch (e: Throwable) {
+                    if (i == 10) {
+                        log.error(e) { }
+                    }
+                }
+            }
+        }
+
         val f = Jsoup.parse(text)
         val names = f.select(".listing-search-item__title").map { it.text() }
         val addresses = f.select(".listing-search-item__sub-title").map { it.text() }
@@ -179,8 +227,24 @@ class ParariusListingsFetcher : ListingsFetcher {
 
 
 @Component
+class DriverFactory {
+
+    fun webDriver(): WebDriver {
+//        FirefoxDriverManager.getInstance().setup()
+        val options = FirefoxOptions()
+//        options.binary = FirefoxBinary(File("./lib/geckodriver"))
+        options.addArguments("--headless")
+        options.addArguments("--window-size=1512,754")
+        options.addArguments("""--user-agent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36"""")
+        return FirefoxDriver(options)
+    }
+}
+
+@Component
 class FundaListingsFetcher : ListingsFetcher {
     override fun fetch(): List<Listing> {
+
+
         val link = "https://www.funda.nl/huur/amsterdam/1500-2000/sorteer-datum-af/"
         val request = HttpRequest.newBuilder().uri(URI.create(link)).header(
             "accept",
@@ -211,5 +275,4 @@ class FundaListingsFetcher : ListingsFetcher {
             )
         }
     }
-
 }
